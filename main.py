df9bc61f507400e7d5fd9324169ee4ca71e0854d
AlkoKovad <sanya.dobriy.sosed@mail.ru>
2020-11-26 21:39:02 +0300

    случайные окружности и git 26.11.2020

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import sys
 from random import randint
 
-from PyQt5 import uic
 from PyQt5.QtGui import QColor, QPainter, QBrush
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
+from UI import Ui_MainWindow
 
-class MainWindow(QMainWindow):
+
+class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
-        super(QMainWindow, self).__init__()
-        uic.loadUi('UI.ui', self)
+        super().__init__()
+        self.setupUi(self)
         self.do_paint = False
         self.pushButton.clicked.connect(self.run)
 
@@ -18,7 +19,8 @@ class MainWindow(QMainWindow):
         self.repaint()
 
     def generate(self, qp):
-        qp.setBrush(QBrush(QColor(255, 255, 0)))
+        qp.setBrush(QBrush(QColor(randint(0, 255), randint(0, 255),
+                                  randint(0, 255))))
         size = randint(0, 600)
         qp.drawEllipse(randint(0, 600), randint(0, 600), size, size)
 
